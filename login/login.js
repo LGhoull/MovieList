ae3b0f0f9beea9be4115ce3c8970d1452af94729
Fittkau Luis <luis.fittkau@d-velop.de>
2023-05-06 12:41:17 +0200

    progress

--- login/login.js
+++ login/login.js
@@ -1,3 +1,12 @@
-function signIn() {
-    
+function signIn(username, password) {
+    if(username == 'test' && password == 'test')
+    {
+        alert('signin successful');
+    }
 }
+
+var signinButton = document.getElementById('signin-button');
+var usernameInput = document.getElementById('username-input');
+var passwordInput = document.getElementById('password-input');
+
+signinButton.addEventListener('click', signIn(usernameInput.value, passwordInput.value));
