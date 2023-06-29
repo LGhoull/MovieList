a82759ce78b042e56159f0e7a4399e4241df3b37
Fittkau Luis <luis.fittkau@d-velop.de>
2023-06-29 21:14:52 +0200

    fix

--- search/liste.php
+++ search/liste.php
@@ -66,38 +66,14 @@
         ?>
 
         <ul class="movie-list">
-            <?php foreach ($data as $movie): ?>
-                <li class="movie-item">
-                    <img class="movie-poster" src=<?php echo $movie['Poster']; ?>>
-                    <div class="movie-details">
-                        <h2 class="movie-title"> 
-                            <a href="details.php?id=<?php echo $movie['imdbID']; ?>" id="movie-title-link">
-                            <?php echo $movie['Title']; ?>
-                            </a>
-                        </h2>
-                        <p class="movie-overview">
-                            <?php echo $movie['Year'];?> • 
-                            
-                            <?php 
-                                if($movie['Type'] == 'movie') {
-                                    echo 'Film';
-                                } elseif ($movie['Type'] == 'series') {
-                                    echo 'Serie';
-                                } elseif ($movie['Type'] == 'game') {
-                                    echo 'Spiel';
-                                } else {
-                                    echo 'Sonstiges';
-                                }
-                            ?> • imdb-Bewertung 
-                            <?php 
-                                $movieData = json_decode(file_get_contents("https://www.omdbapi.com/?apikey=" . $api_key . "&i=" . $movie['imdbID']), true);
-                                echo $movieData['imdbRating'];
-                                echo "<br>" . $movieData['Plot'];
-                            ?>
-                        </p>
-                    </div>
-                </li>
-            <?php endforeach; ?>
+            <?php foreach ($data as $movie){
+                echo "<li class=\"movie-item\">                    <img class=\"movie-poster\" src=" . $movie['Poster'] . " >                    <div class=\"movie-details\">                        <h2 class=\"movie-title\">                             <a href=\"details.php?id=" . $movie['imdbID'] . "\" id=\"movie-title-link\">                            " . $movie['Title'] . "                            </a>                        </h2>                        <p class=\"movie-overview\">                            " . $movie['Year']; . " • "
+                if($movie['Type'] == 'movie') {echo 'Film';} elseif ($movie['Type'] == 'series') {echo 'Serie';} elseif ($movie['Type'] == 'game') {echo 'Spiel';} else {echo 'Sonstiges';}
+                echo " • imdb-Bewertung"                             
+                $movieData = json_decode(file_get_contents("https://www.omdbapi.com/?apikey=" . $api_key . "&i=" . $movie['imdbID']), true);                                echo $movieData['imdbRating'];                                echo "<br>" . $movieData['Plot'];
+                echo "</p>                    </div>                </li>"
+            }
+            ?>
         </ul>
     </body>
 </html>
